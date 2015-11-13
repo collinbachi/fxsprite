7c3f80ab56320d32138edcfbcabe4bd67f9c31a2
Collin Bachi <collinbachi@gmail.com>
2015-11-12 19:50:00 -0500

    initial commit

---
+++ src/fxsprite/Sprite.java
@@ -0,0 +1,16 @@
+package fxsprite;
+
+import javafx.geometry.Bounds;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Sprite extends ImageView{
+	
+	public Sprite(Image sheet, Bounds frame){
+		//TODO
+	}
+	
+	public Sprite(Image sheet, int width, int height){
+		//TODO
+	}
+}
